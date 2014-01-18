769f5d0c75dbd8b2116e6952acc1ba6f831868e4
Jesse Farmer <jessethebuilder@yahoo.com>
2014-01-17 23:12:22 -0800

    Added Facebook Social Plugin

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -1,6 +1,6 @@
 module FacebookHelper
   def fb_social_plugin(url, width = 700, fb_id = FACEBOOK_ID, color = 'light')
-    render :partial => '/app/views/facebook_helper/_social_plugin.html.erb',
+    render :partial => 'facebook_helper/social_plugin',
            :locals => {:url => url, :width => width, :fb_id => fb_id, :color => color}
   end
 end
