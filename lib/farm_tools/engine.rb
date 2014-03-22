4fff5aed2d5c97ddba3bd7b506ff6cd281677267
Jesse Farmer <jessethebuilder@yahoo.com>
2014-03-22 16:42:56 -0700

    Migrations

--- lib/farm_tools/engine.rb
+++ lib/farm_tools/engine.rb
@@ -1,6 +1,6 @@
 module FarmTools
   class Engine < ::Rails::Engine
-    isolate_namespace EngineWithMigrations
+    #isolate_namespace EngineWithMigrations
 
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
