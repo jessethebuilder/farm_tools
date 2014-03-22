7677e1fe55caa2149663965d123d020fc34e8df5
Jesse Farmer <jessethebuilder@yahoo.com>
2014-03-22 16:34:03 -0700

    Migrations

--- lib/farm_tools/engine.rb
+++ lib/farm_tools/engine.rb
@@ -1,5 +1,15 @@
 module FarmTools
   class Engine < ::Rails::Engine
+    isolate_namespace EngineWithMigrations
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+
     config.generators do |g|
       g.test_framework :rspec, :fixture => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
