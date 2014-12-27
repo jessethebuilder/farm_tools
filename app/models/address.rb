64b609d7f4418ae906bb3872d9692eb1372f1674
Jesse Farmer <jessethebuilder@yahoo.com>
2014-12-26 16:42:09 -0800

    Updates to Address

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ActiveRecord::Base
   belongs_to :has_address, :polymorphic => true
+
+  def to_s
+    s = ''
+    [:street, :street2, :street3, :city, :state, :zip].each do |sym|
+      s += "#{send(sym)}, " if send(sym)
+    end
+    s.length > 2 ? s.chop.chop : s
+  end
 end
