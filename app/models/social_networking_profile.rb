4527fa97a4e32d41bc8625ddf9e1ec93515319d0
Jesse Farmer <jessethebuilder@yahoo.com>
2015-03-05 02:34:49 -0800

    Refactor Facebook Helpers

--- app/models/social_networking_profile.rb
+++ app/models/social_networking_profile.rb
@@ -5,7 +5,7 @@ class SocialNetworkingProfile < ActiveRecord::Base
     #this and similar methods are designed to take a url in, parse it, and save the id
     #if the id is known, simply save it in the proper column (such as :facebook_id)
     val =~ /https?:\/\/www\.facebook\.com\/([^?^\/]+)/
-    #raise ArgumentError, "#{val} is not recognized as a valid Facebook url" if $~.nil?
+    raise ArgumentError, "#{val} is not recognized as a valid Facebook url" if $~.nil?
     write_attribute(:facebook_id, $1)
   end
 
@@ -14,27 +14,35 @@ class SocialNetworkingProfile < ActiveRecord::Base
   end
 
   def facebook_icon(image_url, use_text_label: false)
-    # html = %Q|<a href="#{facebook_url}" target="_blank">|
-    # html += %Q|<img src="#{image_url}" alt="Facebook">|
-    # html += " Facebook" if use_text_label
-    # html += '</a>'
-    # html.html_safe
-
     social_networking_icon('facebook', image_url, :use_text_label => use_text_label)
   end
 
   def twitter_url=(val)
-    val =~ /https?:\/\/www\.twitter\.com\/([^?^\/]+)/
-    #raise ArgumentError, "#{val} is not recognized as a valid Twitter url" if $~.nil?
+    val =~ /https?:\/\/twitter\.com\/([^?^\/]+)/
+    #val /=~ /https?:\/\/www\.twitter\.com\/([^?^\/]+)/
+    raise ArgumentError, "#{val} is not recognized as a valid Twitter url" if $~.nil?
     write_attribute(:twitter_id, $1)
   end
 
   def twitter_url
-    twitter_id ? "https://www.twitter.com/#{twitter_id}" : nil
+    twitter_id ? "https://twitter.com/#{twitter_id}" : nil
   end
 
   def twitter_icon(image_path, use_text_label: false)
     social_networking_icon('twitter', image_path, :use_text_label => use_text_label)
+  end
+
+  def pinterest_url=(val)
+    val =~ /https?:\/\/www\.pinterest\.com\/([^?^\/]+)/
+    write_attribute(:pinterest_id, $1)
+  end
+
+  def pinterest_url
+    pinterest_id ? "https//www.pinterest.com/#{pinterest_id}" : nil
+  end
+
+  def pinterest_icon(image_path, use_text_label: false)
+    social_networking_icon('pinterest', image_path, :use_text_label => use_text_label)
   end
 
   def tumblr_url=(val)
