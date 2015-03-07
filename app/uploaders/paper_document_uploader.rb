39ca0e0746d9808f840be448e940ac37799035e2
Jesse Farmer <jessethebuilder@yahoo.com>
2015-03-06 19:18:40 -0800

    Fix FarmSlug save bug

---
+++ app/uploaders/paper_document_uploader.rb
@@ -0,0 +1,55 @@
+# encoding: utf-8
+
+class PaperDocumentUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  storage :file
+  # storage :fog
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Provide a default URL as a default if there hasn't been a file uploaded:
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+  # end
+
+  version :full do
+    process :resize_to_fit => [927, 1200]
+  end
+
+  version :preview do
+    process :resize_to_fit => [387, 500]
+  end
+
+  version :thumb do
+    process :resize_to_fit => [58, 75]
+  end
+
+  # Process files as they are uploaded:
+  # process :scale => [200, 300]
+  #
+  # def scale(width, height)
+  #   # do something
+  # end
+
+  # Create different versions of your uploaded files:
+  # version :thumb do
+  #   process :resize_to_fit => [50, 50]
+  # end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+  # Override the filename of the uploaded files:
+  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  # def filename
+  #   "something.jpg" if original_filename
+  # end
+
+end
