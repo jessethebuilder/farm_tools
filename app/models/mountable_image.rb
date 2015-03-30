c65a85107c56d81723b06574e23c7bc940b5e183
Jesse Farmer <jessethebuilder@yahoo.com>
2015-03-29 18:27:40 -0700

    MountableImage

--- app/models/mountable_image.rb
+++ app/models/mountable_image.rb
@@ -1,5 +1,5 @@
 class MountableImage < ActiveRecord::Base
   belongs_to :has_mountable_images, polymorphic: true
 
-  mount_uploader :image, MountableImage
+  mount_uploader :image, MountableImage, :ignore_integrity_errors => true
 end
